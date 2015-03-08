b2e6605b87fd65a7598b6bf2978011f062a95341
Jon H.M. Chan <me@jonhmchan.com>
2015-03-08 14:47:21 -0400

    Remove corrupted wordpress image import files

--- iterator.py
+++ iterator.py
@@ -1,4 +1,4 @@
-import os, re
+import os, re, requests
 rootdir = '_posts'
 
 for subdir, dirs, files in os.walk(rootdir):
@@ -13,10 +13,19 @@ for subdir, dirs, files in os.walk(rootdir):
     	for key, line in enumerate(contents):
     		src = re.search('\!\[.*\]\((.*)\)', line)
     		if src:
+    			wordpress_src = re.search('http://blog.stackoverflow.com/wp-content/uploads/(.*)', src.group(1))
+    			if wordpress_src:
+    				print 'Retrieving ' + src.group(1) + '...'
+	    			image_src = wordpress_src.group(1)
+	    			f = open('images/wordpress/'+image_src, "w")
+	    			f.write(requests.get(src.group(1)).content)
+	    			f.close()
+    			continue
     			folder = file.replace(".markdown", "")
     			x = line.replace(str("/blog/images/" + folder), "http://blog.stackoverflow.com/wp-content/uploads")
     			contents[key] = x
 
+    	continue
     	f = open(filename, "w")
     	contents = "".join(contents)
     	f.write(contents)
