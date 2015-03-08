e17efdda6710b9aaa63955f8ad8de2641e05ce07
Jon H.M. Chan <me@jonhmchan.com>
2015-03-08 14:56:25 -0400

    Wordpress image import

--- iterator.py
+++ iterator.py
@@ -11,21 +11,13 @@ for subdir, dirs, files in os.walk(rootdir):
 
     	# Find first image
     	for key, line in enumerate(contents):
-    		src = re.search('\!\[.*\]\((.*)\)', line)
+    		src = re.search('\!\[.*\]\((.*?)\)', line)
     		if src:
     			wordpress_src = re.search('http://blog.stackoverflow.com/wp-content/uploads/(.*)', src.group(1))
     			if wordpress_src:
-    				print 'Retrieving ' + src.group(1) + '...'
-	    			image_src = wordpress_src.group(1)
-	    			f = open('images/wordpress/'+image_src, "w")
-	    			f.write(requests.get(src.group(1)).content)
-	    			f.close()
-    			continue
-    			folder = file.replace(".markdown", "")
-    			x = line.replace(str("/blog/images/" + folder), "http://blog.stackoverflow.com/wp-content/uploads")
-    			contents[key] = x
+	    			x = line.replace(str("/blog/images/wordpress"), "http://blog.stackoverflow.com/wp-content/uploads")
+	    			contents[key] = x
 
-    	continue
     	f = open(filename, "w")
     	contents = "".join(contents)
     	f.write(contents)
