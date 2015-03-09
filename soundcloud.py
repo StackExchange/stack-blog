9359e76752584f89d6d4ac03b8934d06519fb8e7
Jon Chan <jchan@stackoverflow.com>
2015-03-09 17:33:19 -0400

    Better hero

---
+++ soundcloud.py
@@ -0,0 +1,42 @@
+import os, re, requests
+rootdir = '_posts'
+
+for subdir, dirs, files in os.walk(rootdir):
+    for file in files:
+        filename = os.path.join(subdir, file)
+
+    	f = open(filename, "r")
+    	contents = f.readlines()
+    	f.close()
+
+    	# Get WordPress 
+    	slug = filename.replace("_posts/", "").replace(".markdown", "")
+
+    	splits = slug.split("-")
+    	year = splits[0]
+    	month = splits[1]
+    	end = "-".join(splits[3:])
+
+    	link = "/".join([year, month, end])
+    	link = "/" + link
+    	print link
+
+    	contents.insert(5, "redirect_from: " + link)
+    	f = open(filename, "w")
+    	f.write("".join(contents))
+    	f.close()
+
+    	continue
+    	for key, line in enumerate(contents):
+    		src = re.search('\!\[.*?\]\((.*?)\)', line)
+    		if src:
+    			wordpress_src = re.search('/blog/images/wordpress/(.*)', src.group(1))
+    			if wordpress_src:
+	    			image_src = wordpress_src.group(1)
+	    			path = 'images/wordpress/'+image_src
+	    			print 'Retrieving ' + path + '...'
+	    			if not os.path.isfile(path):
+	    				print path
+		    			f = open(path, "w")
+		    			f.write(requests.get("http://blog.stackoverflow.com/wp-content/uploads/" + wordpress_src.group(1)).content)
+		    			f.close()
