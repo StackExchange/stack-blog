9d5e8d69a53299835a23a72ffebcf9b4fcd0c558
Jon Chan <jchan@stackoverflow.com>
2015-03-09 20:13:05 -0400

    Implement javascript pagination for home page

--- js/index.js
+++ js/index.js
@@ -1,13 +1,17 @@
 $(document).ready(function() {
 
-	if (!check_page()) {
-		set_page(1);
+	var page;
+
+	if ($("section.page").length > 0) {
+		if (!check_page()) {
+			set_page(1);
+		}
 	}
 
 	if ($("section.page").length > 0) {
 		$("a.page").click(function() {
-			var page_num;
-			if (page_num = parseInt($(this).attr("href").split("#page")[1])) {
+			var page_num = ($(this).hasClass("older") ? page + 1 : page - 1);
+			if (valid_page(page_num)) {
 				set_page(page_num);
 			}
 		});
@@ -22,7 +26,6 @@ $(document).ready(function() {
 			var matched = href.match('^#page');
 			if (matched) {
 				var page_num = href.split("#page")[1];
-				alert(page_num);
 				if (page_num = parseInt(page_num)) {
 					set_page(page_num);
 					return true;
@@ -33,12 +36,23 @@ $(document).ready(function() {
 	}
 
 	function set_page(page_num) {
+		page = page_num;
 		$("section.page").removeClass("active");
 		$("section.page#page_" + page_num.toString()).addClass("active");
 
-		var previous = page_num - 1;
+		window.location.hash = "#page" + page_num.toString();
+
 		var next = page_num + 1;
-		$("a.page.older").attr("href", "#page" + next.toString()).css("visibility", ((next > $("article.post").length) ? "none" : "visible"));
-		$("a.page.newer").attr("href", "#page" + previous.toString()).css("visibility", ((previous < 0) ? "none" : "visible"));
+		var previous = page_num - 1;
+
+		$("span.page.total_pages").html(Math.floor($("article.post").length / 5));
+		$("span.page.current_page").html(page_num);
+
+		$("a.page.older").css("visibility", (!valid_page(next) ? "hidden" : "visible"));
+		$("a.page.newer").css("visibility", (!valid_page(previous) ? "hidden" : "visible"));
+	}
+
+	function valid_page(page_num) {
+		return (page_num <= Math.floor($("article.post").length / 5) && (page_num > 0));
 	}
 })
