f26c2059ff6e2a595097ef7a03efe149f9e253eb
Jon Chan <jchan@stackoverflow.com>
2015-03-10 12:38:18 -0400

    Add default images for podcasts if necessary

--- iterator.py
+++ iterator.py
@@ -10,22 +10,10 @@ for subdir, dirs, files in os.walk(rootdir):
     	f.close()
 
     	# Find first image
-    	for key, line in enumerate(contents):
-    		src = re.search('\!\[.*?\]\((.*?)\)', line)
-    		if src:
-    			wordpress_src = re.search('/blog/images/wordpress/(.*)', src.group(1))
-    			if wordpress_src:
-	    			image_src = wordpress_src.group(1)
-	    			path = 'images/wordpress/'+image_src
-	    			print 'Retrieving ' + path + '...'
-	    			if not os.path.isfile(path):
-	    				print path
-		    			f = open(path, "w")
-		    			f.write(requests.get("http://blog.stackoverflow.com/wp-content/uploads/" + wordpress_src.group(1)).content)
-		    			f.close()
-
-    	continue
-    	f = open(filename, "w")
-    	contents = "".join(contents)
-    	f.write(contents)
-    	f.close()
+        if re.search('podcast', filename):
+            if re.search('^hero: ', contents[6]):
+                print filename
+                contents.insert(6, 'hero: /blog/images/category/podcasts.jpg\n')
+                f = file.open(filename, "w")
+                f.write("".join(contents))
+                f.close()
