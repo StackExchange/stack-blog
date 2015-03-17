02cd230974fea0b77a447eed24a8160011a69d22
Jon Chan <jchan@stackoverflow.com>
2015-03-17 14:37:20 -0400

    AJAX pagination for front page

--- js/index.js
+++ js/index.js
@@ -1,58 +1,79 @@
 $(document).ready(function() {
 
 	var page;
+	var data;
 
-	if ($("section.page").length > 0) {
-		if (!check_page()) {
-			set_page(1);
-		}
-	}
-
-	if ($("section.page").length > 0) {
-		$("a.page").click(function() {
-			var page_num = ($(this).hasClass("older") ? page + 1 : page - 1);
-			if (valid_page(page_num)) {
-				set_page(page_num);
-			}
-		});
+	if ($("div.pagination").length > 0) {
+		page = 1;
+		check_page();
+	} else {
+		$(".posts").css("visibility", "visible");
 	}
 
 	function check_page() {
 		var href = window.location.hash;
-		console.log(href);
-		var pages = $("section.page");
-		var post_count = $("article.post").length;
-		if (pages.length > 0) {
-			var matched = href.match('^#page');
-			if (matched) {
-				var page_num = href.split("#page")[1];
-				if (page_num = parseInt(page_num)) {
-					set_page(page_num);
-					return true;
-				}
+		var matched = href.match('^#page');
+		if (matched) {
+			var page_num = href.split("#page")[1];
+			if (page_num = parseInt(page_num)) {
+				page = page_num;
 			}
 		}
-		return false;
+		$.get("/blog/json/index.json", function(response) {
+			if (response) {
+				data = response;
+
+				set_page(page);
+
+				// Set total pages
+				$("span.page.total_pages").html(Math.floor(data.posts.length / 5));
+
+				// Bind page clicks
+				$("a.page").click(function() {
+					var selected_page = ($(this).hasClass("older") ? page + 1 : page - 1);
+					if (valid_page(selected_page)) {
+						set_page(selected_page);
+					}
+				});
+			}
+		});
 	}
 
 	function set_page(page_num) {
+		if (data == null) return;
 		page = page_num;
-		$("section.page").removeClass("active");
-		$("section.page#page_" + page_num.toString()).addClass("active");
 
 		window.location.hash = "#page" + page_num.toString();
 
 		var next = page_num + 1;
 		var previous = page_num - 1;
 
-		$("span.page.total_pages").html(Math.floor($("article.post").length / 5));
 		$("span.page.current_page").html(page_num);
 
-		$("a.page.older").css("visibility", (!valid_page(next) ? "hidden" : "visible"));
-		$("a.page.newer").css("visibility", (!valid_page(previous) ? "hidden" : "visible"));
+		var offset = (page_num - 1) * 5;
+
+		for (var i = 0; i < 5; i++) {
+			var post = data.posts[offset + i];
+			var author = data.authors[post.author];
+			var article = $("article.post:eq(" + i + ")");
+
+			article.find(".title").html(post.title).attr("href", post.url);
+			article.find(".avatar").attr("src", author.avatar);
+			article.find(".author-link").html(author.name).attr("href", author.url);
+			article.find(".twitter-link").html(author.twitter ? "@" + author.twitter : "").attr("href", "http://twitter.com/" + author.twitter);
+			article.find(".date").html(post.date);
+
+			article.find("img.hero").attr("src", post.hero);
+			article.find("a.hero_url").attr("href", post.url);
+
+			article.find(".excerpt").html(post.content);
+			article.find("a.read-more").attr("href", post.url);
+		}
+
+		$(".posts").css("visibility", "visible");
 	}
 
 	function valid_page(page_num) {
-		return (page_num <= Math.max(Math.floor($("article.post").length / 5) && (page_num > 0)), 1);
+		return (page_num > 0 && page_num <= Math.max(Math.floor(data.posts.length / 5), 1))
 	}
 })
