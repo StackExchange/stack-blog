79aa92bf35a102b84145a30bd902802aa87082dc
Jon Chan <jchan@stackoverflow.com>
2015-03-17 15:30:15 -0400

    Updated ajax pagination for all categories

--- js/index.js
+++ js/index.js
@@ -21,12 +21,26 @@ $(document).ready(function() {
 		}
 		$.get("/blog/json/index.json", function(response) {
 			if (response) {
+
+				if (typeof channel !== 'undefined') {
+					var result = []
+					for (key in response.posts) {
+						var post = response.posts[key];
+						if (post.categories.indexOf(channel) > - 1) {
+							result.push(post);
+						}
+					}
+					response.posts = result;
+				}
+
 				data = response;
+
+				console.log(data);
 
 				set_page(page);
 
 				// Set total pages
-				$("span.page.total_pages").html(Math.floor(data.posts.length / 5));
+				$("span.page.total_pages").html(Math.max(Math.floor(data.posts.length / 5), 1));
 
 				// Bind page clicks
 				$("a.page").click(function() {
@@ -54,20 +68,32 @@ $(document).ready(function() {
 
 		for (var i = 0; i < 5; i++) {
 			var post = data.posts[offset + i];
-			var author = data.authors[post.author];
 			var article = $("article.post:eq(" + i + ")");
+			article.css("border-bottom", i == data.posts.length - 1 ? "none" : null);
+			if (i >= data.posts.length || typeof post == 'undefined') {
+				article.css("display", "none");
+				continue;
+			}
+
+			var author = data.authors[post.author];
 
 			article.find(".title").html(post.title).attr("href", post.url);
 			article.find(".avatar").attr("src", author.avatar);
+
+			var author_info = [];
+
+			if (author.twitter) author_info.push('<a href="http://twitter.com/' + author.twitter + '">@' + author.twitter + '</a>');
+			author_info.push(post.date);
+
 			article.find(".author-link").html(author.name).attr("href", author.url);
-			article.find(".twitter-link").html(author.twitter ? "@" + author.twitter : "").attr("href", "http://twitter.com/" + author.twitter);
-			article.find(".date").html(post.date);
+			article.find(".author-info").html(author_info.join(" &bull; "));
 
 			article.find("img.hero").attr("src", post.hero);
 			article.find("a.hero_url").attr("href", post.url);
 
 			article.find(".excerpt").html(post.content);
 			article.find("a.read-more").attr("href", post.url);
+			article.css("display", "block");
 		}
 
 		$(".posts").css("visibility", "visible");
@@ -76,4 +102,4 @@ $(document).ready(function() {
 	function valid_page(page_num) {
 		return (page_num > 0 && page_num <= Math.max(Math.floor(data.posts.length / 5), 1))
 	}
-})
+});
